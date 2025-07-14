a75055a21acc862c12823280d957946df7d885dd
Shashwot Adhikari <shashwot123@gmail.com>
2025-07-14 12:44:16 +0545

    Add buttons for adding and removing data
    Add buttons for add, delete and changing read status

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 // Array to store all books
-const myLibrary = [];
+var myLibrary = [];
 
 // Constructor for Book object
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.read = read ? 'Yes' : 'No';
+    
     this.id = crypto.randomUUID(); // Creates a unique id for each book
     // To return info of the book
     this.info = function(){
         return this.id + '.' + this.title + ' by ' 
             + this.author + ', ' + this.pages + ' pages, ' 
-            + (read?'read':'not read yet');
+            + this.read;
     }
+}
+
+// Prototype function for Book
+Book.prototype.changeReadStatus = function() {
+    // Toggle read based on current value
+    this.read = this.read === 'Yes' ? 'No' : 'Yes'
 }
 
 // Function to get book info from user and create the book object
@@ -21,18 +28,25 @@ function addBookToLibrary(title, author, pages, read){
     myLibrary.push(new Book(title, author, pages, read));
 }
 
+// Function to remove book from the library
+function removeBookFromLibrary(id){
+    myLibrary = myLibrary.filter(book => book.id !== id);
+    displayAllBooks();
+}
+
 addBookToLibrary('A', 'B', 'C', true);
-addBookToLibrary('New', 'Book', 'Chaaa', true);
+addBookToLibrary('New', 'Book', 'Chaaa', false);
 addBookToLibrary('Aasdkjasbkd', 'Bpasdjoas', 'asdasd', true);
-addBookToLibrary('Akahsdjkasd', 'Bkjasdjkas', 'Casdhasd', true);
+addBookToLibrary('Akahsdjkasd', 'Bkjasdjkas', 'Casdhasd', false);
 addBookToLibrary('A', 'B', 'C', true);
-addBookToLibrary('New', 'Book', 'Chaaa', true);
+addBookToLibrary('New', 'Book', 'Chaaa', false);
 addBookToLibrary('Aasdkjasbkd', 'Bpasdjoas', 'asdasd', true);
-addBookToLibrary('Akahsdjkasd', 'Bkjasdjkas', 'Casdhasd', true);
+addBookToLibrary('Akahsdjkasd', 'Bkjasdjkas', 'Casdhasd', false);
 displayAllBooks();
 
 // Display all the books in the library
 function displayAllBooks(){
+    document.querySelector('#container').innerHTML = '';
     myLibrary.forEach(book => {
         const container = document.querySelector('#container');
         const card = document.createElement('div');
@@ -57,18 +71,57 @@ function displayAllBooks(){
         cardRead.classList.toggle('card-items');
         card.appendChild(cardRead);
 
+        const readButton = document.createElement('button');
+        readButton.textContent = 'Change Read Status';
+        // Change read status on button click
+        readButton.addEventListener('click', () => {
+            book.changeReadStatus();
+            displayAllBooks();
+        })
+
+        card.appendChild(readButton);
+
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Remove';
+        cardRead.classList.toggle('card-items','remove-button');
+        removeButton.addEventListener('click', () => {
+            // Pass id of the book that is to be removed
+            removeBookFromLibrary(book.id);
+        })
+
+        card.appendChild(removeButton);
+
         card.classList.toggle('card');
         container.appendChild(card);
     })
 }
 
+// Show dialog on button click
 const dialog = document.querySelector('#dialog');
 const addButton = document.querySelector('.add');
-addButton.addEventListener('click', () =>{
+addButton.addEventListener('click', () => {
     dialog.showModal();
 })
 
+// Close dialog on button click
 const closeButton = document.querySelector('.close');
-closeButton.addEventListener('click', () =>{
+closeButton.addEventListener('click', () => {
     dialog.close();
+})
+
+// Handle the data after form submission
+const submitButton = document.querySelector('.submit');
+
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const read = document.querySelector('#read').checked;
+
+    addBookToLibrary(title, author, pages, read);
+
+    dialog.close();
+    displayAllBooks();
 })
